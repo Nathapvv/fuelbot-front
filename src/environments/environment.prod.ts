a547ad3b1e9ed3a6336a557e9a61d0c051721e32
Nathanael <nathanael.reich@gmail.com>
2025-06-29 18:53:00 +0200

    new env prod conf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,24 @@
 export const environment = {
-  production: true,
+  // Config de l'application NON MODIFIABLE
+  production: false,
   appName: 'Fuelbot',
   appVersion: '1.0.0',
   apiUrl: 'https://fuelbot-back-production.up.railway.app/api',
+  mapKey: 'AfhVmSOCE8MNjkQSXqdZ',
+  // Config popup
+  toastTimer: 1500,
+  toastPosition: 'top-end',
+  toastTimerProgressBar: true,
+  // Config de la map
+  mapZoom: 18,
+  mapMinZoom: 15,
+  mapMaxZoom: 18,
+  mapDarkThemeName: 'dataviz',
+  mapLightThemeName: 'bright',
+  mapTitleSize: 256,
+  mapUserPinSize: 0.05,
+  mapStationPinSize: 0.05,
+  mapPinAnchorX: 0.5,
+  mapPinAnchorY: 1,
 };
+
